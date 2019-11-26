5d8931f8d0261c612886aa7bd9bbc90d1bbe8573
Fred Kaesmann <fredkaesmann@UDSAdmins-MacBook-Pro.local>
2019-11-26 08:52:37 -0500

    cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const whitelist = [
   "https://booz-api.herokuapp.com",
   "https://booz-app.surge.sh/",
   "https://booz-app.surge.sh",
-  "http://booz-app.surge.sh/"
+  "http://booz-app.surge.sh"
 ];
 const corsOptions = {
   origin: (origin, callback) => {
